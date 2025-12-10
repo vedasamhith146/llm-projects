270aa6fde3f1d2d55766c6af289676c1d43c6d38
vedasamhith1406 <vedasamhithg@gmail.com>
2025-12-10 11:41:10 +0530

    Add third version of BPE tokenizer(mostly correct, just a few changes required)

--- 01-tokenization-embeddings/tokenizer.py
+++ 01-tokenization-embeddings/tokenizer.py
@@ -1,10 +1,11 @@
 with open("data/tiny.txt",'r') as file:
     text=file.read()
 text=list(text.encode('UTF-8'))
-len_text=len(text)
 vocab_size=256
 vocab_max=vocab_size-1
-for x in range(10):
+while vocab_size<=400:
+    len_text=len(text)
+    print("length of text is",len_text)
     vocab={}
     for x,y in zip(text,text[1:]):
         key=(x,y)
@@ -12,7 +13,6 @@ for x in range(10):
     P=list(vocab.items())
     P.sort(key= lambda p:p[1], reverse=True)
     (p1,p2)=(P[0])[0]
-    print("the highest frequency is of",(P[0])[0],"and it's value is ",(P[0])[1])
     vocab_size+=1
     vocab_max+=1
     i=0
@@ -24,6 +24,10 @@ for x in range(10):
         else :
             new_text.append(text[i])
             i+=1
+    if i!=len_text:
+        new_text.append(text[i])
+    text=new_text.copy()
+print("final length of text is",len(text))
 
 
     
