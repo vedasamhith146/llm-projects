82d8ca92233d05a97a13bbb4792c614a43ca7756
vedasamhith1406 <vedasamhithg@gmail.com>
2025-12-11 11:06:03 +0530

    made small changes to visualizer

--- 01-tokenization-embeddings/visualizer.py
+++ 01-tokenization-embeddings/visualizer.py
@@ -1,15 +1,12 @@
 from bpe_decode import decode
 from bpe_encode import encode
 
-#def visualizer(text):
-    #text=encode(text)
-    #print("Text with token boundaries")
-    #for i in range(len(text)):
-        #print(f"[{text[i]}]:{decode(text[i])}")
+def visualizer(text):
+    text=encode(text)
+    print("Text with token boundaries")
+    for i in range(len(text)):
+        print(f"[{text[i]}]:{decode([text[i]])}")
 
 with open("data/testnew.txt",'r') as f:
     new=f.read()
-new=encode(new)
-print("Text with token boundaries")
-for i in range(len(new)):
-    print(f"{new[i]}:{decode([new[i]])}")
+visualizer(new)
