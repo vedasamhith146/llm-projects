77cdf90f01c67135071421fac8fc8c71e04f15b0
vedasamhith1406 <vedasamhithg@gmail.com>
2025-12-17 11:21:20 +0530

    successfully completed sinusoidal positional embeddings

--- p02_positional_embeddings/sinusoidal.py
+++ p02_positional_embeddings/sinusoidal.py
@@ -1,12 +1,29 @@
 import torch 
 import math
-def pos_embd_sin(pos,d):
-    pe=[]
-    for i in range(d):
-        if i%2==0:
-            pe.append(math.sin(pos/pow(10000,2*i/d)))
-        else:
-            pe.append(math.cos(pos/pow(10000,2*i/d)))
-    pe=torch.tensor(pe)
-    return pe
-print(pos_embd_sin(1,16))
+from token_tensor import get_tokens,token_embd
+def pos_embd_sin(no_of_pos,d):
+    w_pe=[]
+    for m in range(no_of_pos):
+        pe=[]
+        for n in range(d):
+            i=n//2
+            if n%2==0:
+                pe.append(math.sin(m/pow(10000,2*i/d)))
+            else:
+                pe.append(math.cos(m/pow(10000,2*i/d)))
+        w_pe.append(pe)
+    w_pe=torch.tensor(w_pe)
+    return w_pe 
+
+no_of_pos=token_embd.size(0)
+pos_embd=pos_embd_sin(no_of_pos,16)
+total_embeddings_sinusoidal=token_embd+pos_embd
+
+
+
+
+
+
+
+
+
