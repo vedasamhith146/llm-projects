71105f09bfd32936537c0d7e938cfdf79d92f063
vedasamhith1406 <vedasamhithg@gmail.com>
2025-12-19 14:36:50 +0530

    corrected learned positional embeddings code

--- p02_positional_embeddings/learned.py
+++ p02_positional_embeddings/learned.py
@@ -3,17 +3,14 @@ from token_tensor import token_embd
 from bpe_encode import encode
 g=torch.Generator()
 g.manual_seed(42)
-def pos_embd_learned(vocab_size,dim,idx):
-    vec=torch.randn((vocab_size,dim),generator=g)
-    vec=vec[idx]
-    return vec
-
-
+dim=token_embd.size(1)
+P=torch.randn((400,dim),generator=g)
 with open("data/test.txt",'r') as f:
     text=f.read()
 text=encode(text)
 
-pos_embd=pos_embd_learned(1264,16,text)
+T=token_embd.size(0)
+pos_embd=P[torch.arange(T)]
 total_embeddings_learned=token_embd+pos_embd
 
 
