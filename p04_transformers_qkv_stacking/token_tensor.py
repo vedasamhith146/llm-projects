5e8229cd697dc2aa7f7d69d3cb4aebc39dbc8ccd
vedasamhith1406 <vedasamhithg@gmail.com>
2025-12-27 15:48:07 +0530

    changed the dataset

--- p04_transformers_qkv_stacking/token_tensor.py
+++ p04_transformers_qkv_stacking/token_tensor.py
@@ -1,12 +1,13 @@
 import torch
 from bpe_encode import encode
 g=torch.Generator()
-g.manual_seed(123)
-def get_tokens(text,d):
+g.manual_seed(42)
+d=16
+lookup_table=torch.randn((1264,d),generator=g)
+def get_tokens(text):
     text=(encode(text))
-    lookup_table=torch.randn((1264,d),generator=g)
     tokens=lookup_table[text]
     return tokens
 with open("data/test.txt") as f:
     text=f.read()
-token_embd=get_tokens(text,16)
+token_embd=get_tokens(text)
