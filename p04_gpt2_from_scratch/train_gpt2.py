2a8f83f8f4765927997e197eae2b45d485bf87cd
vedasamhith1406 <vedasamhithg@gmail.com>
2026-01-10 14:27:17 +0530

    Implemented custom initialization for Linear and Embedding layers using a normal distribution with std=0.02 to ensure training stability.

--- p04_gpt2_from_scratch/train_gpt2.py
+++ p04_gpt2_from_scratch/train_gpt2.py
@@ -74,6 +74,18 @@ class GPT2(nn.Module):
     ))
     self.lm_head=nn.Linear(config.n_embd,config.vocab_size,bias=False)
     self.transformer.wte.weight=self.lm_head.weight
+
+    self.apply(self._init_weights)
+
+  def _init_weights(self,module):
+     if isinstance(module,nn.Linear):
+        torch.nn.init.normal_(module.weight,mean=0.0,std=0.02)
+        if module.bias is not None:
+           torch.nn.init.zeros_(module.bias)
+     elif isinstance(module,nn.Embedding):
+        torch.nn.init.normal_(module.weight,mean=0.0,std=0.02)
+
+    
   def forward(self,idx,targets=None):
     B,T=idx.size()
     assert T<=self.config.block_size, f"Cannot forward sequence of length {T}"
