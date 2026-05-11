9df5dc90e465ec64473060c5d24cd54bbea3cc6d
vedasamhith1406 <vedasamhithg@gmail.com>
2026-05-11 22:54:03 +0530

    made few minor changes

--- p04_gpt2_from_scratch/exp.py
+++
@@ -1,8 +0,0 @@
-from transformers import GPT2LMHeadModel
-
-# Load GPT-2 (124M)
-model = GPT2LMHeadModel.from_pretrained("gpt2")
-
-# Print all named parameters
-for name, param in model.named_parameters():
-    print(name, param.shape)
