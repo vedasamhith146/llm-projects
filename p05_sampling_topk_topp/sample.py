d9ef2cdd64d5e4f488f27d28ee02f4814552fe03
vedasamhith1406 <vedasamhithg@gmail.com>
2026-05-13 19:21:56 +0530

    Final version of sampling code for B=1

--- p05_sampling_topk_topp/sample.py
+++ p05_sampling_topk_topp/sample.py
@@ -1,84 +1,78 @@
 import torch
-from torch.nn import functional as F
-from train_gpt2 import GPT2, GPT2Config, device 
+import torch.nn.functional as F
+from train_gpt2 import GPT2,GPT2Config
+import tiktoken
+import time
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f"Using device: {device}")
+device='mps' if torch.backends.mps.is_available() else 'cpu'
 
+checkpoint="gpt2_step_11500.pt"
+state_dict=torch.load(checkpoint,map_location=device)
 
-checkpoint_path = "gpt2_step_11500.pt" 
+for k in list(state_dict.keys()):
+    if k.startswith('_orig_mod.'):
+        state_dict[k[len('_orig_mod.'):]]=state_dict.pop(k)
 
-config = GPT2Config(vocab_size=50304)
-model = GPT2(config)
+model=GPT2(GPT2Config(vocab_size=50304))
+model.to(device)
 
-state_dict = torch.load(checkpoint_path, map_location=device)
-unwanted_prefix = '_orig_mod.'
-for k, v in list(state_dict.items()):
-    if k.startswith(unwanted_prefix):
-        state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
 model.load_state_dict(state_dict)
 
-model.to(device)
-model.eval() 
+enc=tiktoken.get_encoding('gpt2')
 
-def generate(model,prompt, max_tokens,top_k,top_p,temp):
-    import tiktoken
-    enc = tiktoken.get_encoding('gpt2')
-    tokens = enc.encode(prompt)
-    tokens = torch.tensor(tokens, dtype=torch.long, device=device).unsqueeze(0)
 
-    tokens_entropy_history=[]
-    generated_text=prompt
+def next_token_generator(logits,temp,top_k,top_p):
+    if temp==0:
+        next_token=logits.argmax(dim=-1,keepdim=True)
+        return next_token
+    logits=logits/temp
 
-    for _ in range(max_tokens):
-        with torch.no_grad():
-            tokens_cond=tokens[:,-1024:]
-            logits, _ = model(tokens_cond)
-            logits = logits[:, -1, :] 
+    if top_k is not None:
+        _,topk_indices = torch.topk(logits,k=top_k)
+        mask=torch.ones_like(logits,dtype=torch.bool)
+        mask[0,topk_indices[0]]=False
+        logits[mask]=-float('inf')
 
-            if temp==0:
-                next_token=torch.argmax(logits,dim=-1).unsqueeze(0).unsqueeze(0)
-                tokens_entropy_history.append(0.0)
-            else:
-                logits=logits/temp
-                if top_k>0:
-                    #v, _ = torch.topk(logits, top_k)
-                    v,_=torch.sort(logits,descending=True)
-                    v=v[:,:top_k]
-                    logits[logits < v[:, [-1]]] = -float('Inf')
-                probs = F.softmax(logits, dim=-1)
 
-                if top_p>0:
-                    sorted_probs,sorted_indices=torch.sort(probs,descending=True)
-                    cumm_probs=torch.cumsum(sorted_probs,dim=-1)
-                    sorted_indices_to_remove=cumm_probs>top_p
-                    sorted_indices_to_remove[:,1:]=sorted_indices_to_remove[:,:-1].clone()
-                    sorted_indices_to_remove[:, 0] = False
-                    sorted_indices_remove=sorted_indices[sorted_indices_to_remove]
-                    logits[0,sorted_indices_remove]=-float('Inf')
-                    probs=F.softmax(logits,dim=-1)
+    if top_p is not None:
+        probs=F.softmax(logits,dim=-1)
+        sorted_probs,sorted_indices=torch.sort(probs,descending=True) 
+        cumm_prob=torch.cumsum(sorted_probs,dim=-1)
+        sorted_indices_to_remove=cumm_prob > top_p
+        sorted_indices_to_remove[:,1:]=sorted_indices_to_remove[:,:-1].clone()
+        sorted_indices_to_remove[:,0]=False
+        gather_indices=sorted_indices[~sorted_indices_to_remove].unsqueeze(0)
+        mask=torch.ones_like(logits,dtype=torch.bool)
+        mask[0,gather_indices[0]]=False
+        logits[mask]=-float('inf')
 
-                current_entropy = -(probs * torch.log(probs + 1e-9)).sum().item()
-                tokens_entropy_history.append(current_entropy)
-
-                next_token = torch.multinomial(probs, num_samples=1)
-
-            tokens = torch.cat((tokens, next_token), dim=1)
-            word=enc.decode(next_token[0].tolist())
-            generated_text+=word
-
-    return generated_text,tokens_entropy_history
-
-print("Model loaded! Type your prompt below (or type 'exit' to quit).")
-
-while True:
-    user_input = input("\nPrompt: ")
+    probs=F.softmax(logits,dim=-1)
+    next_token=torch.multinomial(probs,num_samples=1)
+    return next_token
     
-    if user_input.lower() in ['exit', 'quit']:
-        break
-        
-    if user_input.strip() == "":
-        continue
 
-    text,entropy=generate(model,user_input, 120,40,0.9,0.8)
-    print("\nGenerated:\n",text)
+def generate_text(prompt,max_new_tokens,temp=0.7,top_k=50,top_p=0.90):
+
+    initial_tokens=enc.encode(prompt)
+    tokens=torch.tensor(initial_tokens,dtype=torch.long,device=device).unsqueeze(0) #shape (1,T)
+    
+
+    with torch.no_grad():
+        for _ in range(max_new_tokens):
+            logits,_=model(tokens)
+            logits=logits[:,-1,:]   
+            next_token=next_token_generator(logits,temp=temp,top_k=top_k,top_p=top_p)                        #shape(1,50304)
+            tokens=torch.cat((tokens,next_token),dim=-1)
+    
+    all_tokens=tokens.squeeze(0).tolist()
+    generated_tokens=all_tokens[len(initial_tokens):]
+    generated_text=enc.decode(generated_tokens)
+
+
+    print("Output Generated from the model:")
+    print(f"{generated_text}")
+
+if __name__=="__main__":
+    prompt="The Industrial Revolution happened in"
+
+    generate_text(prompt,max_new_tokens=50)
