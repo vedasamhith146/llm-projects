5197e41557b38fe0f6893638bf16767a17e5e6cc
Veda samhith <githubveda@gmail.com>
2026-06-05 16:10:54 +0000

    Added new files

--- positional-encoding-benchmarks/analysis.py
+++ positional-encoding-benchmarks/analysis.py
@@ -5,6 +5,7 @@ from learned import learned_config,learned_model
 from sinusoidal import sinusoidal_config,sinusoidal_model,SinusoidalPositionalEmbedding
 from rope import RoPE_config,RoPE_model,RotationalPositionalEmbedding
 from alibi import ALiBi_config,ALiBi_model,Attention_Mask
+from without_position import without_position_config,without_position_model
 
 device= "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -12,6 +13,7 @@ learned_model=learned_model(learned_config()).to(device)
 sinusoidal_model=sinusoidal_model(sinusoidal_config()).to(device)
 RoPE_model=RoPE_model(RoPE_config()).to(device)
 ALiBi_model=ALiBi_model(ALiBi_config()).to(device)
+without_position_model=without_position_model(without_position_config()).to(device)
 
 batch_size=16
 
@@ -37,6 +39,7 @@ learned_model_dict=torch.load("learned_model.pt")
 sinusoidal_model_dict=torch.load("sinusoidal_model.pt")
 RoPE_model_dict=torch.load("RoPE_model.pt")
 ALiBi_model_dict=torch.load("ALiBi_model.pt")
+without_position_model_dict=torch.load("without_position_model.pt")
 
 for key in list(learned_model_dict.keys()):
     if key.startswith('_orig_mod.'):
@@ -54,10 +57,15 @@ for key in list(ALiBi_model_dict.keys()):
     if key.startswith('_orig_mod.'):
         ALiBi_model_dict[key[len('_orig_mod.'):]]=ALiBi_model_dict.pop(key)
 
+for key in list(without_position_model_dict.keys()):
+    if key.startswith('_orig_mod.'):
+        without_position_model_dict[key[len('_orig_mod.'):]]=without_position_model_dict.pop(key)
+
 learned_model.load_state_dict(learned_model_dict)
 sinusoidal_model.load_state_dict(sinusoidal_model_dict)
 RoPE_model.load_state_dict(RoPE_model_dict)
 ALiBi_model.load_state_dict(ALiBi_model_dict,strict=False)
+without_position_model.load_state_dict(without_position_model_dict)
 
 block_size_learned=[32,64,128]
 block_size_others=[32,64,128,256,512,1024]
@@ -66,11 +74,13 @@ learned_model.eval()
 sinusoidal_model.eval()
 RoPE_model.eval()
 ALiBi_model.eval()
+without_position_model.eval()
 
 perplexity_learned=[]
 perplexity_sinusoidal=[]
 perplexity_RoPE=[]
 perplexity_ALiBi=[]
+perplexity_without_position=[]
 
 for block_size in block_size_learned:
     per=estimate_perplexity("val",learned_model,block_size)
@@ -88,22 +98,30 @@ for block_size in block_size_others:
     per=estimate_perplexity("val",ALiBi_model,block_size)
     perplexity_ALiBi.append(per)
 
+for block_size in block_size_others:
+    per=estimate_perplexity("val",without_position_model,block_size)
+    perplexity_without_position.append(per)
+
+
+
 print("Learned:", perplexity_learned)
 print("Sinusoidal:", perplexity_sinusoidal)
 print("RoPE:", perplexity_RoPE)
 print("ALiBi:", perplexity_ALiBi)
+print("Without position:",perplexity_without_position)
 
-
+plt.plot(block_size_learned,perplexity_learned,label="Learned")
 plt.plot(block_size_others, perplexity_sinusoidal, label="Sinusoidal")
 plt.plot(block_size_others, perplexity_RoPE, label="RoPE")
 plt.plot(block_size_others, perplexity_ALiBi, label="ALiBi")
+plt.plot(block_size_others,perplexity_without_position,label="Without position")
 
 plt.yscale("log")
 
 plt.xlabel("Context Length")
 plt.ylabel("Perplexity(Log scale)")
 plt.legend()
-plt.savefig("context_length vs perplexity_log.png",dpi=300)
+plt.savefig("context_length vs perplexity_log_2.png",dpi=300)
 
 
 
